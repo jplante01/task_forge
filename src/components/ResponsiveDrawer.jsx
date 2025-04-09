cfab0f320dacfbbd7c4331ce4cc802ec33803c46
Jeffrey Plante <git@jplante.dev>
2025-04-09 08:14:26 -0700

    Start Drawer

--- src/components/ResponsiveDrawer.jsx
+++ src/components/ResponsiveDrawer.jsx
@@ -1,11 +1,62 @@
 import { Drawer, Stack, Typography } from "@mui/material";
+import * as React from "react";
+
 
 export default function ResponsiveDrawer() {
-    return (
-      <Drawer open={true} anchor="left" variant="permanent">
-        <Stack>
-          <Typography>TasksLogo</Typography>
-        </Stack>
-      </Drawer>
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [isClosing, setIsClosing] = React.useState(false);
+
+    const handleDrawerClose = () => {
+      setIsClosing(true);
+      setMobileOpen(false);
+    };
+
+    const handleDrawerTransitionEnd = () => {
+      setIsClosing(false);
+    };
+
+    const handleDrawerToggle = () => {
+      if (!isClosing) {
+        setMobileOpen(!mobileOpen);
+      }
+    };
+
+    const drawer = (
+      <Stack>
+        <Typography>TasksLogo</Typography>
+      </Stack>
     );
+  return (
+    <>
+      {/* Mobile Drawer */}
+      <Drawer
+        // container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onTransitionEnd={handleDrawerTransitionEnd}
+        onClose={handleDrawerClose}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: "240px" },
+        }}
+      >
+        {drawer}
+      </Drawer>
+
+      {/* Desktop Drawer */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: "240px" },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </>
+  );
 }
