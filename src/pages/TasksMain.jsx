34bce83fd26a052609e1bf8a3e87df7670fd54a1
Jeffrey P <jeffp.web@gmail.com>
2025-04-09 12:42:13 -0700

    Fix error

--- src/pages/TasksMain.jsx
+++ src/pages/TasksMain.jsx
@@ -14,7 +14,7 @@ const projectsList = [
 export default function TasksMain() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
-  const [projectsLists, setProjectsLists] = React.useState(projectsList);
+  const [projectsLists] = React.useState(projectsList);
   // const [selectedProjectIndex, setSelectedProjectIndex] = React.useState(0);
 
   const handleDrawerClose = () => {
