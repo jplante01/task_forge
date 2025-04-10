68bd739060be7618a2cdc862458dde4a6f161b9c
Jeffrey Plante <git@jplante.dev>
2025-04-10 10:49:10 -0700

    Create AddTaskForm

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,11 +1,12 @@
 import { TextField } from "@mui/material";
-
+import * as React from "react";
 export default function AddTaskForm() {
   return (
     <TextField
       label="Add Task"
       variant="outlined"
       size="small"
+      sx={{ width: "200px" }}
     />
   );
 }
