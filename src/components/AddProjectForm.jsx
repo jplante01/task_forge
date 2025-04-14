be37b81d63ee2ac5133114ed90ed03fb7b3e77ac
Jeffrey Plante <git@jplante.dev>
2025-04-14 10:44:11 -0700

    Create AddProjectForm

---
+++ src/components/AddProjectForm.jsx
@@ -0,0 +1,8 @@
+import { TextField } from "@mui/material";
+import * as React from "react";
+
+export default function AddProjectForm() {
+  return (
+    <TextField label="Add Project" variant="outlined" size="small" fullWidth />
+  );
+}
