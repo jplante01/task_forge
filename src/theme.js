2189676e3dd3c9e99c716cde1399663d451ceaa5
Jeffrey P <jeffp.web@gmail.com>
2025-04-14 09:22:00 -0700

    Install fontsource and create the logoFont variant

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 import { red, grey } from '@mui/material/colors';
+import "@fontsource/permanent-marker";
 
 // Create a theme instance.
 const theme = createTheme({
   cssVariables: true,
+  typography: {
+      logoFont: {
+      fontFamily: "Permanent Marker",
+    },
+  },
   palette: {
     primary: {
       main: '#556cd6',
