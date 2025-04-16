a113a17f15e865542c70b26dd7fec959bb1c434d
Jeffrey P <jeffp.web@gmail.com>
2025-04-16 13:50:17 -0700

    Add the queryClient provider and the UIStateProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,23 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
-
+import { UIStateProvider } from './contexts/UIStateContext';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <UIStateProvider>
+          <App />
+        </UIStateProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
