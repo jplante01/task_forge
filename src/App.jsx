433ec960872bfaac2ec85749b793bd7058b90f9e
Jeffrey Plante <git@jplante.dev>
2025-04-16 13:51:24 -0700

    Remove  doubled theme provider error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import TasksMain from './pages/TasksMain';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
+
+
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
       <TasksMain />
-    </ThemeProvider>
   );
 }
