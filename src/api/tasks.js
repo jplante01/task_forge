25cae9c9daa77cea5278c95358f1f548bca7b699
Jeffrey P <jeffp.web@gmail.com>
2025-04-22 09:45:19 -0700

    Change imports

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -1,4 +1,4 @@
-import { supabase } from "./supabase";
+import supabase from "../lib/supabase";
 export const tasksApi = {
   getTasksByProjectId: async (projectId) => {
     const { data, error } = await supabase
