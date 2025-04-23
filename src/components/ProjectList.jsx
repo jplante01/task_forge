c6e9e306fa26649a796961251556ec7d2ab83a31
Jeffrey P <jeffp.web@gmail.com>
2025-04-23 13:49:17 -0700

    Connect local dev db to pull projects

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,22 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
-import { projects as projectsData } from "../data/seedData";
 import { List, Typography } from "@mui/material";
 import ProjectListItem from "./ProjectListItem";
 import React, { useContext } from "react";
 import { UIStateContext } from "../contexts/UIStateContext";
+import projectsApi from "../api/projects";
 
 export default function ProjectList() {
+  const user = { id: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa" };
   const { setSelectedProject, selectedProject } = useContext(UIStateContext);
   const {
     data: projects,
     isLoading,
     isError,
   } = useQuery({
-    queryKey: ["projects"],
-    queryFn: () =>
-      new Promise((resolve) =>
-        setTimeout(() => resolve([...projectsData]), 1000),
-      ),
+    queryKey: ["projects", user?.id],
+    queryFn: () => projectsApi.getProjectsByUserId(user?.id),
   });
   return (
     <List>
