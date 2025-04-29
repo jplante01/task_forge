3853ad5ae129cd99e277f5ae1884eb78a5650004
Jeffrey Plante <git@jplante.dev>
2025-04-29 13:19:14 -0700

    Add delete project functionality

--- src/components/ProjectOptionsMenu.jsx
+++ src/components/ProjectOptionsMenu.jsx
@@ -7,8 +7,9 @@ import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
-function ProjectOptionsMenu({ project }) {
+function ProjectOptionsMenu({ project, onDeleteProject }) {
   const [anchorEl, setAnchorEl] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState("");
@@ -30,6 +31,14 @@ function ProjectOptionsMenu({ project }) {
 
   const handleModalClose = () => {
     setModalOpen(false);
+  };
+
+  const handleConfirmDelete = () => {
+    // Call the parent component's delete function
+    if (onDeleteProject) {
+      onDeleteProject(project.id);
+    }
+    handleModalClose();
   };
 
   // Modal style
@@ -56,7 +65,6 @@ function ProjectOptionsMenu({ project }) {
       >
         <MoreVertIcon />
       </IconButton>
-
       <Menu
         id="long-menu"
         anchorEl={anchorEl}
@@ -65,14 +73,14 @@ function ProjectOptionsMenu({ project }) {
       >
         <MenuItem
           onClick={() =>
-            handleModalOpen(`Are you sure you want to delete project "${project.name}?"`)
+            handleModalOpen(
+              `Are you sure you want to delete project "${project.name}"?`,
+            )
           }
         >
           Delete Project
         </MenuItem>
-
       </Menu>
-
       <Modal
         open={modalOpen}
         onClose={handleModalClose}
@@ -81,14 +89,24 @@ function ProjectOptionsMenu({ project }) {
       >
         <Box sx={modalStyle}>
           <Typography id="modal-title" variant="h6" component="h2">
-            Project Details
+            Confirm Deletion
           </Typography>
-          <Typography id="modal-description" sx={{ mt: 2, mb: 2 }}>
+          <Typography id="modal-description" sx={{ mt: 2, mb: 3 }}>
             {modalContent}
           </Typography>
-          <Button onClick={handleModalClose} variant="contained" size="small">
-            Close
-          </Button>
+          <Stack direction="row" spacing={2} justifyContent="flex-end">
+            <Button onClick={handleModalClose} variant="outlined" size="small">
+              Cancel
+            </Button>
+            <Button
+              onClick={handleConfirmDelete}
+              variant="contained"
+              color="error"
+              size="small"
+            >
+              Delete
+            </Button>
+          </Stack>
         </Box>
       </Modal>
     </div>
