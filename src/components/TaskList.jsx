3c048f069f4bbbf40b5967cb3fb7a9bf97473c13
Jeffrey Plante <git@jplante.dev>
2025-04-29 16:41:25 -0700

    Implement completed checkbox function

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -16,31 +16,67 @@ import AddTaskForm from "./AddTaskForm";
 import { useContext } from "react";
 import { UIStateContext } from "../contexts/UIStateContext";
 
-function ActiveTaskItem({ task }) {
+function ActiveTaskItem({
+  task,
+  toggleTaskComplete,
+  toggleTaskStarred,
+  deleteTask,
+}) {
   return (
     <Stack direction="row" spacing={1}>
       <ListItem disablePadding divider>
         <ListItemIcon sx={{ minWidth: 0 }}>
-          <Checkbox disableRipple checked={task.completed} />
+          <Checkbox
+            disableRipple
+            checked={task.completed}
+            onChange={() =>
+              toggleTaskComplete.mutate({
+                id: task.id,
+                completed: task.completed,
+              })
+            }
+          />
         </ListItemIcon>
         <ListItemText primary={task.title} />
         <IconButton disableRipple edge="end" aria-label="star">
-          <StarIcon  sx={{ color: task.starred ? "yellow" : "grey.400", "&:hover": { color: "grey.500" } }} />
+          <StarIcon
+            sx={{
+              color: task.starred ? "yellow" : "grey.400",
+              "&:hover": { color: "grey.500" },
+            }}
+          />
         </IconButton>
       </ListItem>
       <IconButton disableRipple aria-label="delete" size="small">
-        <ClearIcon sx={{ color: "grey.300", "&:hover": { color: "grey.500" } }} />
+        <ClearIcon
+          sx={{ color: "grey.300", "&:hover": { color: "grey.500" } }}
+        />
       </IconButton>
     </Stack>
   );
 }
 
-function CompletedTaskItem({ task }) {
+function CompletedTaskItem({
+  task,
+  toggleTaskComplete,
+  toggleTaskStarred,
+  deleteTask,
+}) {
   return (
     <Stack direction="row" spacing={1}>
       <ListItem disablePadding divider>
         <ListItemIcon sx={{ minWidth: 0 }}>
-          <Checkbox disableRipple color="gray.500" checked={task.completed} />
+          <Checkbox
+            disableRipple
+            color="gray.500"
+            checked={task.completed}
+            onChange={() =>
+              toggleTaskComplete.mutate({
+                id: task.id,
+                completed: task.completed,
+              })
+            }
+          />
         </ListItemIcon>
         <ListItemText primary={task.title} sx={{ color: "grey.500" }} />
         <IconButton disableRipple edge="end" aria-label="star">
@@ -62,31 +98,55 @@ function CompletedTaskItem({ task }) {
 }
 
 export default function TaskList() {
-  const { tasks } = useContext(UIStateContext);
+  const { tasks, toggleTaskComplete, toggleTaskStarred, deleteTask } =
+    useContext(UIStateContext);
 
   if (tasks.length === 0) {
     return <Typography>No project selected</Typography>;
   }
   const completedTasks = tasks.filter((task) => task.completed);
   const activeTasks = tasks.filter((task) => !task.completed);
-  console.log(activeTasks);
   return (
     <Stack direction="column" spacing={0}>
       <AddTaskForm />
-      <Typography pt={4} variant="subtitle1" sx={{ color: "grey.800", fontWeight: "bold" }}>Active Tasks</Typography>
-    {activeTasks.length === 0 ? (
-      <Typography>No active tasks</Typography>
-    ) : (
-      <List>
-        {activeTasks.map((task) => (
-          <ActiveTaskItem key={task.id} task={task} />
-        ))}
-      </List>
+      <Typography
+        pt={4}
+        variant="subtitle1"
+        sx={{ color: "grey.800", fontWeight: "bold" }}
+      >
+        Active Tasks
+      </Typography>
+      {activeTasks.length === 0 ? (
+        <Typography>No active tasks</Typography>
+      ) : (
+        <List>
+          {activeTasks.map((task) => (
+            <ActiveTaskItem
+              key={task.id}
+              task={task}
+              toggleTaskComplete={toggleTaskComplete}
+              toggleTaskStarred={toggleTaskStarred}
+              deleteTask={deleteTask}
+            />
+          ))}
+        </List>
       )}
-      <Typography pt={4} variant="subtitle1" sx={{ color: "grey.800", fontWeight: "bold" }}>Completed Tasks</Typography>
+      <Typography
+        pt={4}
+        variant="subtitle1"
+        sx={{ color: "grey.800", fontWeight: "bold" }}
+      >
+        Completed Tasks
+      </Typography>
       <List>
         {completedTasks.map((task) => (
-          <CompletedTaskItem key={task.id} task={task} />
+          <CompletedTaskItem
+            key={task.id}
+            task={task}
+            toggleTaskComplete={toggleTaskComplete}
+            toggleTaskStarred={toggleTaskStarred}
+            deleteTask={deleteTask}
+          />
         ))}
       </List>
     </Stack>
