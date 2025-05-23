387381bb87f404561598654aa94893d1e18b4277
Jeffrey P <jeffp.web@gmail.com>
2025-05-23 16:46:51 -0700

    uncomment to AddProjectForm to bring back in

--- src/components/AddProjectForm.jsx
+++ src/components/AddProjectForm.jsx
@@ -1,43 +1,42 @@
-// import React, { useContext, useState } from "react";
-// import { UIStateContext } from "../contexts/UIStateContext";
-// import { TextField, Box, Button } from "@mui/material";
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
-// export default function AddProjectForm() {
-//   const { createProject } = useContext(UIStateContext);
-//   const [name, setName] = useState("");
-//   const [error, setError] = useState(null);
+export default function AddProjectForm() {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(null);
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setError(null);
-//     try {
-//       await createProject.mutateAsync({ name });
-//       setName(""); // Reset form on success
-//     } catch (err) {
-//       setError(err.message);
-//     }
-//   };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setError(null);
+  //   try {
+  //     await createProject.mutateAsync({ name });
+  //     setName(""); // Reset form on success
+  //   } catch (err) {
+  //     setError(err.message);
+  //   }
+  // };
 
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <TextField
-//         label="Project name"
-//         variant="outlined"
-//         size="small"
-//         fullWidth
-//         value={name}
-//         onChange={(e) => setName(e.target.value)}
-//         disabled={createProject.isLoading}
-//       />
-//       <Button
-//         type="submit"
-//         variant="text"
-//         color="primary"
-//         disabled={createProject.isLoading || !name}
-//       >
-//         {createProject.isLoading ? "Creating..." : "+ Add Project"}
-//       </Button>
-//       {error && <div style={{ color: "red" }}>{error}</div>}
-//     </form>
-//   );
-// }
+  return (
+    <form onSubmit={() => {}}>
+      <TextField
+        label="Project name"
+        variant="outlined"
+        size="small"
+        fullWidth
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        // disabled={createProject.isLoading}
+      />
+      <Button
+        type="submit"
+        variant="text"
+        color="primary"
+        // disabled={createProject.isLoading || !name}
+      >
+        Add Project
+        {/* {createProject.isLoading ? "Creating..." : "+ Add Project"} */}
+      </Button>
+      {error && <div style={{ color: "red" }}>{error}</div>}
+    </form>
+  );
+}
