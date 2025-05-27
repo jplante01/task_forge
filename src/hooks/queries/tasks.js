0b727d1a1aaefbd210f71bbae615865aad80df8c
Jeffrey P <jeffp.web@gmail.com>
2025-05-26 20:21:28 -0700

    Implement task deletion

--- src/hooks/queries/tasks.js
+++ src/hooks/queries/tasks.js
@@ -1,10 +1,69 @@
 import tasksApi from "../../api/tasks";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 export const useGetTasksByProjectId = (projectId, options = {}) => {
   return useQuery({
     queryKey: ["tasks", projectId],
     queryFn: () => tasksApi.getTasksByProjectId(projectId),
     ...options,
+  });
+};
+
+export const useDeleteTaskById = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data) => {
+      const { taskId, projectId } = data;
+      return tasksApi.deleteTask(taskId, projectId);
+    },
+    onMutate: async (data) => {
+      const { taskId, projectId } = data;
+
+      await queryClient.cancelQueries({ queryKey: ["tasks", projectId] });
+
+      const previousTasks = queryClient.getQueryData(["tasks", projectId]);
+
+      queryClient.setQueryData(["tasks", projectId], (old) => {
+        return old.filter((task) => task.id !== taskId);
+      });
+
+      return { previousTasks };
+    },
+    onError: () => {
+      queryClient.setQueryData(["tasks"], (old) => old);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["tasks"]);
+    },
+  });
+}
+export const useCreateTask = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data) => {
+      const { projectId, taskData } = data;
+      return tasksApi.createTask(projectId, taskData);
+    },
+    onMutate: async (data) => {
+      const { projectId, taskData } = data;
+
+      await queryClient.cancelQueries({ queryKey: ["tasks", projectId] });
+
+      const previousTasks = queryClient.getQueryData(["tasks", projectId]);
+
+      queryClient.setQueryData(["tasks", projectId], (old) => {
+        return [...old, taskData];
+      });
+
+      return { previousTasks };
+    },
+    onError: () => {
+      queryClient.setQueryData(["tasks"], (old) => old);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(["tasks"]);
+    },
   });
 };
