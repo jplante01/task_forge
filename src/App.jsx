fe7a62fbfcdc392e4a465d89fda0eeb9d7116b10
Jeffrey Plante <git@jplante.dev>
2025-05-29 15:44:29 -0700

    restructure app and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import * as React from 'react';
-import TasksMain from './pages/TasksMain';
+import * as React from "react";
+import TasksMain from "./pages/TasksMain";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
 export default function App() {
   return (
-      <TasksMain />
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <QueryClientProvider client={queryClient}>
+            <TasksMain />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
