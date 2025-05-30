142b649a4ca147cd98308751ca2ebfbdf978b907
Jeffrey Plante <git@jplante.dev>
2025-05-30 11:08:28 -0700

    comment out the global query options

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-      staleTime: 1000 * 60 * 5, // 5 minutes
-    },
-    mutations: {
-      retry: false,
-    },
-  },
+  // defaultOptions: {
+  //   queries: {
+  //     refetchOnWindowFocus: false,
+  //     retry: false,
+  //     staleTime: 1000 * 60 * 5, // 5 minutes
+  //   },
+  //   mutations: {
+  //     retry: false,
+  //   },
+  // },
 });
 export default function App() {
   return (
