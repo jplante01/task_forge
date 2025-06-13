4053e7fb51e12b1e50ef1163f97ae6c45b87d13c
Jeffrey P <jeffp.web@gmail.com>
2025-06-13 15:44:45 -0700

    Extract SignInForm

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,190 +1,22 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
-import {
-  Box,
-  Button,
-  TextField,
-  Typography,
-  Container,
-  Avatar,
-  FormControlLabel,
-  Checkbox,
-  Grid,
-  Link,
-} from "@mui/material";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        jplante.dev
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import React from "react";
+import SignInForm from "../components/SignInForm";
+import { Card, CardContent, Box } from "@mui/material";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const { signIn } = useAuth();
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      await signIn(email, password);
-      navigate("/");
-    } catch (error) {
-      console.error("Login error:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    // <Container component="main" maxWidth="xs">
-    //   <Box
-    //     sx={{
-    //       marginTop: 8,
-    //       display: "flex",
-    //       flexDirection: "column",
-    //       alignItems: "center",
-    //     }}
-    //   >
-    //     <Paper
-    //       elevation={3}
-    //       sx={{
-    //         padding: 4,
-    //         display: "flex",
-    //         flexDirection: "column",
-    //         alignItems: "center",
-    //         width: "100%",
-    //       }}
-    //     >
-    //       <Typography component="h1" variant="h5">
-    //         Sign in
-    //       </Typography>
-    //       <Box
-    //         component="form"
-    //         onSubmit={handleSubmit}
-    //         sx={{ mt: 1, width: "100%" }}
-    //       >
-    //         <TextField
-    //           margin="normal"
-    //           required
-    //           fullWidth
-    //           id="email"
-    //           label="Email Address"
-    //           name="email"
-    //           autoComplete="email"
-    //           autoFocus
-    //           value={email}
-    //           onChange={(e) => setEmail(e.target.value)}
-    //         />
-    //         <TextField
-    //           margin="normal"
-    //           required
-    //           fullWidth
-    //           name="password"
-    //           label="Password"
-    //           type="password"
-    //           id="password"
-    //           autoComplete="current-password"
-    //           value={password}
-    //           onChange={(e) => setPassword(e.target.value)}
-    //         />
-    //         <Button
-    //           type="submit"
-    //           fullWidth
-    //           variant="contained"
-    //           sx={{ mt: 3, mb: 2 }}
-    //           disabled={loading}
-    //         >
-    //           {loading ? "Signing in..." : "Sign In"}
-    //         </Button>
-    //       </Box>
-    //     </Paper>
-    //   </Box>
-    // </Container>
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password" 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      <Copyright sx={{ mt: 8, mb: 4 }} />
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {/* <Card variant="outlined"> */}
+        {/* <CardContent> */}
+          <SignInForm />
+        {/* </CardContent> */}
+      {/* </Card> */}
+    </Box>
   );
 }
