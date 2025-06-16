07c7315689815573b9bfe2db89bb7f10ce6f28a5
Jeffrey Plante <git@jplante.dev>
2025-06-16 12:13:41 -0700

    Begin implementing PasswordResetForm

--- src/components/PasswordResetForm.jsx
+++ src/components/PasswordResetForm.jsx
@@ -1,5 +1,95 @@
-import React from "react";
+import * as React from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useAuth } from "../contexts/AuthContext";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 export default function PasswordResetForm() {
-  return <div>PasswordResetForm</div>;
+  const { resetPassword } = useAuth();
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const password1 = data.get("password-1");
+    const password2 = data.get("password-2");
+    if (password1 !== password2) {
+      setError("Passwords do not match");
+    } else {
+      resetPassword(password1);
+      setError(null);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Reset Password{" "}
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            type="password"
+            id="password-1"
+            label="Enter your new password"
+            name="email"
+            // autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Re-enter your new password"
+            type="password"
+            id="password-2"
+            // autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Reset Password{" "}
+          </Button>
+        </Box>
+      </Box>
+      <Copyright sx={{ mt: 8, mb: 4 }} />
+    </Container>
+  );
 }
