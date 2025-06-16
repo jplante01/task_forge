2c1b4a66ca3b73d4a3f0a8706898e9c70625398d
Jeffrey P <jeffp.web@gmail.com>
2025-06-16 11:57:59 -0700

    Add a password reset route and page

--- src/components/PasswordResetDialog.jsx
+++ src/components/PasswordResetDialog.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
@@ -8,54 +9,95 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import { useAuth } from "../contexts/AuthContext";
 
-
 export default function PasswordResetDialog({ dialogOpen, toggleDialog }) {
-
   const { resetPassword } = useAuth();
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
   const handleClose = () => {
+    setShowConfirmation(false);
     toggleDialog();
   };
-  return (
-    <React.Fragment>
 
-      <Dialog
-        open={dialogOpen}
-        onClose={handleClose}
-        PaperProps={{
-          component: "form",
-          onSubmit: (event) => {
-            event.preventDefault();
-            const formData = new FormData(event.currentTarget);
-            const formJson = Object.fromEntries(formData.entries());
-            const email = formJson.email;
-            resetPassword(email);
-            toggleDialog();
-          },
-        }}
-      >
-        <DialogTitle>Reset Password</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter your email address here to reset your password.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button type="submit">Request Reset</Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
-  );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const formJson = Object.fromEntries(formData.entries());
+    const email = formJson.email;
+    resetPassword(email);
+    setShowConfirmation(true);
+    // toggleDialog();
+  };
+
+  if (!showConfirmation) {
+    return (
+      <React.Fragment>
+        <Dialog
+          open={dialogOpen}
+          onClose={handleClose}
+          PaperProps={{
+            component: "form",
+            onSubmit: handleSubmit,
+          }}
+        >
+          <DialogTitle>Reset Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please enter your email address here to reset your password.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Request Reset</Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Dialog
+          open={dialogOpen}
+          onClose={handleClose}
+          PaperProps={{
+            component: "form",
+            onSubmit: handleSubmit,
+          }}
+        >
+          <DialogTitle>Reset Request sent</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please check your email for a link to reset your password.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="name"
+              name="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+              disabled
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Return to Login</Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+    );
+  }
 }
---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { TaskForgeLogo } from "../logo/TaskForgeLogo";
+import { useThemeModeContext } from "../contexts/ThemeModeContext";
+import PasswordResetForm from "../components/PasswordResetForm";
+import { Stack, Typography, Button, Box } from "@mui/material";
+
+export default function ResetPassword() {
+  const { theme } = useThemeModeContext();
+
+  return (
+    <React.Fragment>
+      <Stack sx={{ height: "100%" }}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          sx={{ padding: { xs: "0.5rem", sm: "1.5rem" }, marginBottom: "2rem" }}
+          justifyContent="space-between"
+        >
+          <Stack direction="row" alignItems="center">
+            <Box
+              sx={{
+                width: { xs: "60px", sm: "80px" },
+                height: { xs: "60px", sm: "80px" },
+                marginRight: "0.5rem",
+              }}
+            >
+              <TaskForgeLogo
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  color: theme.palette.text.main,
+                  // padding: "0.5rem",
+                }}
+              />
+            </Box>
+            <Typography
+              variant="logoFont"
+              sx={{ fontSize: { xs: "1.5rem", sm: "2rem" } }}
+            >
+              TASKFORGE
+            </Typography>
+          </Stack>
+          <Stack direction="row" alignItems="center">
+            <Typography
+              variant="body1"
+              sx={{
+                marginRight: "0.5rem",
+                display: { xs: "none", sm: "block" },
+                color: theme.palette.text.secondary,
+              }}
+            ></Typography>
+            <Button variant="contained" color="primary"></Button>
+          </Stack>
+        </Stack>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            marginTop: "2rem",
+          }}
+        >
+          <PasswordResetForm />
+        </Box>
+      </Stack>
+    </React.Fragment>
+  );
+}
