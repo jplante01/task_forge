b93e0b1326c8ff142cb05cec0ae661e64f56116d
Jeffrey P <jeffp.web@gmail.com>
2025-07-17 14:20:18 -0700

    Fix styling

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -6,7 +6,6 @@ import { Stack, Typography, Button, Box, Link } from "@mui/material";
 
 export default function ResetPassword() {
   const { theme } = useThemeModeContext();
-
   return (
     <React.Fragment>
       <Stack sx={{ height: "100%" }}>
@@ -29,7 +28,7 @@ export default function ResetPassword() {
                   sx={{
                     width: "100%",
                     height: "100%",
-                    color: theme.palette.text.main,
+                    color: theme.palette.text.primary,
                     // padding: "0.5rem",
                   }}
                 />
@@ -41,17 +40,6 @@ export default function ResetPassword() {
             >
               TASKFORGE
             </Typography>
-          </Stack>
-          <Stack direction="row" alignItems="center">
-            <Typography
-              variant="body1"
-              sx={{
-                marginRight: "0.5rem",
-                display: { xs: "none", sm: "block" },
-                color: theme.palette.text.secondary,
-              }}
-            ></Typography>
-            <Button variant="outlined" color="primary"></Button>
           </Stack>
         </Stack>
         <Box
